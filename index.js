45788a8dd19c168fc0aa3005335613bf2d2c7e19
Osama Ibrahim <oimigy@live.com>
2023-11-14 07:45:24 +0200

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRouter from "./routes/user/user.router.js";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
 
